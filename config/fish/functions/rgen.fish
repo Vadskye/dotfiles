43f88b57eebbcff7fa5b507d3db8f1a26dbc2207
Kevin Johnson <vadskye@gmail.com>
2021-01-09 13:59:01 -0800

    update rgen for typescript compilation

--- config/fish/functions/rgen.fish
+++ config/fish/functions/rgen.fish
@@ -1,8 +1,12 @@
-# Defined in /tmp/fish.aB1iFq/rgen.fish @ line 2
+# Defined in /tmp/fish.yJpPnU/rgen.fish @ line 2
 function rgen
+    g rise
+    mkdir -p core_book/generated
 	g rpy
     vf activate rise_book
     latex_generation/gen_all.py
     g rts
-    ns src/generate_latex.js -t monsters -o ../core_book/generated/monster_descriptions.tex
+    npx tsc --incremental
+    npm run script -- src/generate_latex.js -t monsters -o ../core_book/generated/monster_descriptions.tex
+    vf deactivate
 end
